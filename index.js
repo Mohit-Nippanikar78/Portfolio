f3c83810ebe996b3fa2aa563473de58b07adb83d
Mohit <rnippanikar7@gmail.com>
2023-05-15 11:13:18 +0530

    initiial

---
+++ index.js
@@ -0,0 +1,10 @@
+let a = document.querySelectorAll(".menu211")
+function app(){
+    
+}
+let b = a.map (E){
+    if (E.currentTarget.dataset.filter ==="All") {
+        console.log("i a working")
+        
+    }
+}
